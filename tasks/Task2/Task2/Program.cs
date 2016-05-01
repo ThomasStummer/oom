aca9037b3a51127a23c814f667d87c903762d1d4
Thomas Stummer <ic15b079@technikum-wien.at>
2016-05-01 15:07:25 +0200

    Task 2 Drehmaschine implementiert

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void PrintStateOfDrehmaschine(Drehmaschine drehmaschine)
+        {
+            Console.WriteLine("Drehmaschine 1:");
+            Console.WriteLine($"Magazingroesse: {drehmaschine.MagazinGroesse}");
+            for (int i = 0; i < drehmaschine.MagazinGroesse; i++)
+            {
+                Console.WriteLine($"Werkzeug {i}: {drehmaschine.WerkzeugMagazin[i]}");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var drehmaschine = new Drehmaschine(4);
+
+            // Print initial state of Drehmaschine
+            PrintStateOfDrehmaschine(drehmaschine);
+
+            // Werkzeug aufruesten
+            drehmaschine.WerkzeugAufruesten(0, "Schruppmeissel");
+            drehmaschine.WerkzeugAufruesten(1, "Schlichtmeissel");
+            drehmaschine.WerkzeugAufruesten(2, "Gewindeschneider");
+
+            // Print new state of Drehmaschine
+            PrintStateOfDrehmaschine(drehmaschine);
+
+            // Drehen
+            drehmaschine.AktivesWerkzeug = 0;
+            drehmaschine.Drehen();
+
+            // Mit anderem Werkzeug drehen
+            drehmaschine.AktivesWerkzeug = 1;
+            drehmaschine.Drehen();
+
+        }
+    }
+}
