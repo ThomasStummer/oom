aa43be3acb32575bbd73b0615473550c1a5555b2
Thomas Stummer <ic15b079@technikum-wien.at>
2016-05-12 20:51:39 +0200

    Add project Task3

---
+++ tasks/Task3/Task3/Drehmaschine.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Drehmaschine
+    {
+        // Private members
+        readonly int _magazinGroesse;
+        string [] _werkzeugMagazin;        
+        int _aktivesWerkzeug;
+        
+        // Read-only parameter
+        public int MagazinGroesse => _magazinGroesse;
+
+        // Read-only parameter
+        public string[] WerkzeugMagazin => _werkzeugMagazin;
+
+        // Read- and write-able parameter
+        public int AktivesWerkzeug
+        {
+            get { return _aktivesWerkzeug; }
+            set
+            {
+                ValidateMagazinPlatz(value);
+                _aktivesWerkzeug = value;
+            }
+        }
+
+        // Private érror handling function
+        void ValidateMagazinPlatz(int platz)
+        {
+            if (platz < 0 || platz > MagazinGroesse - 1)
+            {
+                throw new Exception("Magazinplatz nicht vorhanden!");
+            }
+        }
+
+        // Constructor
+        public Drehmaschine(int Magazingroesse)
+        {
+            _magazinGroesse = Magazingroesse;
+            _werkzeugMagazin = new string[_magazinGroesse];
+            AktivesWerkzeug = 0;
+        }
+
+        // Public methods
+        public void WerkzeugAufruesten(int MagazinPlatz, string Werkzeug)
+        {
+            ValidateMagazinPlatz(MagazinPlatz);
+            _werkzeugMagazin[MagazinPlatz] = Werkzeug;
+        }
+
+        public void Drehen()
+        {
+            if(string.IsNullOrWhiteSpace(WerkzeugMagazin[AktivesWerkzeug]))
+            {
+                throw new Exception("Kein Werkzeug eingespannt!");
+            }
+            Console.WriteLine($"Werkstück wird mit {_werkzeugMagazin[AktivesWerkzeug]} bearbeitet.");
+        }
+        
+    }
+}
